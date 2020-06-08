6615de1f1aa99df76d63f99865fbf4d25cc97851
PopovaMaria <Geer78@ya.ru>
2020-06-08 22:29:20 +0300

    frog can turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 import arcade
+import os
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
@@ -9,12 +10,69 @@ SPRITE_SCALING_FROG = 0.3
 BUG_COUNT = 5
 MOVEMENT_SPEED = 4
 
+TEXTURE_LEFT = 0
+TEXTURE_RIGHT = 1
+TEXTURE_UP = 2
+TEXTURE_DOWN = 3
+
+class Frog(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.textures = []
+        # Load a left facing texture and a right facing texture.
+        # flipped_horizontally=True will mirror the image we load.
+        texture = arcade.load_texture("images/frog2.png")
+        self.textures.append(texture)
+        texture = arcade.load_texture("images/frog1.png")
+        self.textures.append(texture)
+        texture = arcade.load_texture("images/frog3.png")
+        self.textures.append(texture)
+        texture = arcade.load_texture("images/frog4.png")
+        self.textures.append(texture)
+
+
+        self.scale = SPRITE_SCALING_FROG
+
+        # By default, face right.
+        self.set_texture(TEXTURE_RIGHT)
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Figure out if we should face left or right
+        if self.change_x < 0:
+            self.texture = self.textures[TEXTURE_LEFT]
+        elif self.change_x > 0:
+            self.texture = self.textures[TEXTURE_RIGHT]
+        elif self.change_y > 0:
+            self.texture = self.textures[TEXTURE_DOWN]
+        elif self.change_y < 0:
+            self.texture = self.textures[TEXTURE_UP]
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
 class MyGame(arcade.Window):
     """ Главный класс приложения. """
 
     def __init__(self, width, height):
         super().__init__(width, height)
 
+        # Variables that will hold sprite lists
+        self.bug_list = None
+
+        # Set up the player info
+        self.frog_list = None
         arcade.set_background_color(arcade.color.FRENCH_BEIGE)
 
     def setup(self):
@@ -30,8 +88,7 @@ class MyGame(arcade.Window):
 
             # Задать игрока и
             # Его изображение из kenney.nl
-            self.frog_sprite = arcade.Sprite("images/frog.png",
-                                             SPRITE_SCALING_FROG)
+            self.frog_sprite = Frog()
             self.frog_sprite.center_x = 500  # Стартовая позиция
             self.frog_sprite.center_y = 400
             self.frog_list.append(self.frog_sprite)
@@ -58,8 +115,10 @@ class MyGame(arcade.Window):
         self.frog_list.draw()
         self.bug_list.draw()
 
-    def update(self, delta_time):
+    def on_update(self, delta_time):
         """ Здесь вся игровая логика и логика перемещения."""
+        self.frog_list.update()
+        self.bug_list.update()
         self.physics_engine.update()
 
 
@@ -77,12 +136,14 @@ class MyGame(arcade.Window):
             self.frog_sprite.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Вызывается, когда пользователь отпускает клавишу"""
+        """Called when the user releases a key. """
 
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.frog_sprite.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.frog_sprite.change_x = 0
+
+
 
 
 
